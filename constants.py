2fe32803e7eec7d6199f4c39da6c10dfe3969f18
Thibaud-P <thibaud.pigeon@gmail.com>
2014-04-27 22:25:54 +0200

    More constants

--- constants.py
+++ constants.py
@@ -6,4 +6,10 @@
 ##                                              ##
 ##################################################
 
+import os
 
+WINDOW_ICON_PATH = os.path.join("Images", "Icon.png")
+WINDOW_CAPTION = "Bros. Bomb Squad"
+WINDOW_X_RES = 640
+WINDOW_Y_RES = 480
+WINDOW_RES = (WINDOW_X_RES, WINDOW_Y_RES)
